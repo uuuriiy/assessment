1775c54a1a143479781aaa4a8fcbdb88433b038c
Den <den@jackhealth.ca>
2025-10-16 19:53:32 +0300

    Task1 completed

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,13 +10,13 @@ const loginUser = async (req, res) => {
     const user = await userModel.findOne({ email });
 
     if (!user) {
-      return res.json({ success: false, message: "User does not exist" });
+      return res.status(401).json({ success: false, message: "User does not exist" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.json({ success: false, message: "Invalid credentials" });
+      return res.status(401).json({ success: false, message: "Invalid credentials" });
     }
 
     const token = createToken(user._id);
@@ -38,7 +38,7 @@ const registerUser = async (req, res) => {
     // checking if user already exists
     const exists = await userModel.findOne({ email });
     if (exists) {
-      return res.json({ success: false, message: "User already exists" });
+      return res.status(401).json({ success: false, message: "User already exists" });
     }
 
     // validating email format and password
