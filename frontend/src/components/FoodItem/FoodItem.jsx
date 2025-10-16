1071f265bfcefed49c0bb92fc2d1f3d817e08f12
Den <den@jackhealth.ca>
2025-10-16 20:03:12 +0300

    Task3 completed

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -14,9 +14,11 @@ const FoodItem = ({id,name,price,description,image}) => {
             {
                 !cartItems[id] ? <img className='add' onClick={()=> addToCart(id)} src={assets.add_icon_white}/>: 
                 <div className="food-item-counter">
-                    <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
-                    <p>{cartItems[id]}</p>
-                    <img onClick={()=>addToCart(id)}  src={assets.add_icon_green} alt="" />
+                    <div className='food-item-counter-content'>
+                        <img onClick={()=>removeFromCart(id)} src={assets.remove_icon_red} alt="" />
+                        <p>{cartItems[id]}</p>
+                        <img onClick={()=>addToCart(id)}  src={assets.add_icon_green} alt="" />
+                    </div>
                 </div>
             }
         </div>
